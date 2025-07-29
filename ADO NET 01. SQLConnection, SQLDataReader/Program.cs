a354e7cd556475594c4e1cd63b7fdb1b0b33dc94
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-07-29 11:39:20 +0400

    Ado Lesson 01

--- ADO NET 01. SQLConnection, SQLDataReader/Program.cs
+++ ADO NET 01. SQLConnection, SQLDataReader/Program.cs
@@ -1,6 +1,81 @@
 ﻿#region ADO
 /*
-ADO .NET -  
+ADO .NET -  ActiveX Data Object for .NET
+
+// MMSQL Server, Oracle, OLE DB, ODBC
  
+
+ADO Classes
+ - DBConnection (SqlConnection, ...)
+ - DBCommand
+ - DBDataReader
+ - DBDataAdapter,
+ - ...
+
+ADO Datatype for DB
+ - DataTable
+ - DataSet
+ - ...
+
+Connection modes:
+    - Connnected Mode
+    - Disconnected Mode
 */
+#endregion
+
+#region ConnectionStrings
+// https://www.connectionstrings.com/
+
+// Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+// Data Source=myServerAddress;Initial Catalog=myDataBase;User Id=myUsername;Password=myPassword;
+
+// Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; Trust Server Certificate=True; Application Intent = ReadWrite; Multi Subnet Failover=False;
+
+#endregion
+
+using Microsoft.Data.SqlClient;
+
+SqlConnection sqlConnection = default!;
+
+string connectionString = @"Server=STHQ0124-01; Database=AdoTest; 
+                            User ID=admin; Password=admin; Trust Server Certificate = True;";
+
+
+#region Database Connection with try finally
+//sqlConnection = new(connectionString);
+
+//string insertQuery = @"INSERT INTO Authors(Id, FirstName, LastName)
+//                        VALUES(1, 'Joseph', 'Albahari')";
+
+////SqlCommand command = new SqlCommand(insertQuery, sqlConnection);
+
+//SqlCommand command = new();
+//command.Connection = sqlConnection;
+//command.CommandText = insertQuery;
+
+//try
+//{
+//    sqlConnection.Open();
+//    command.ExecuteNonQuery();
+//}
+//finally
+//{
+//    if (sqlConnection != null) sqlConnection.Close();
+//}
+#endregion
+
+#region Database Connection with using
+//SqlCommand command = new();
+//using (sqlConnection = new(connectionString))
+//{
+//    sqlConnection.Open();
+//    string FirstName = Console.ReadLine()!;
+//    string LastName = Console.ReadLine()!;
+
+//    string insertQuery = @$"INSERT INTO Authors(Id, FirstName, LastName)
+//                            VALUES(3, '{FirstName}', '{LastName}')";
+//    command.Connection = sqlConnection;
+//    command.CommandText = insertQuery;
+//    command.ExecuteNonQuery();
+//}
 #endregion
---
+++ ADO NET 01. Sql Read Data/Program.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.SqlClient;
+
+string connectionString = @"Server=(localdb)\MSSQLLocalDB;
+                            Database=AdoTest;
+                            Integrated Security=SSPI;
+                            Trust Server Certificate=True";
+
+SqlDataReader reader = null!;
+SqlCommand command = null!;
+
+#region Read Data
+//using SqlConnection connection = new SqlConnection(connectionString);
+//command = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+//connection.Open();
+
+//reader = command.ExecuteReader();
+
+//while (reader.Read())
+//{
+//    Console.WriteLine($"{reader[0]}. {reader[1]} {reader[2]}");
+//}
+#endregion
+
+#region Read Data indexer
+//using SqlConnection connection = new SqlConnection(connectionString);
+//command = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+//connection.Open();
+
+//reader = command.ExecuteReader();
+
+//while (reader.Read())
+//{
+//    Console.WriteLine($"{reader["Id"]}. {reader["FirstName"]} {reader["LastName"]}");
+//}
+#endregion
+
+using SqlConnection connection = new SqlConnection(connectionString);
+command = new SqlCommand(@"SELECT * FROM Authors", connection);
+
+connection.Open();
+
+reader = command.ExecuteReader();
+
+bool line = true;
+
+//Console.WriteLine(reader.FieldCount);
+
+while (reader.Read())
+{
+    if (line)
+    {
+        for (int i = 0; i < reader.FieldCount; i++)
+        {
+            Console.Write($"{reader.GetName(i)}\t      ");
+        }
+        line = false;
+    }
+    Console.WriteLine();
+    for (int i = 0; i < reader.FieldCount; i++)
+    {
+        Console.Write($"{reader[i]}\t       ");
+    }    
+}
