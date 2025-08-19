c5b0dfbbaa6452bd5583525cfa43ee81ef528582
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-08-19 11:57:16 +0400

    ADO 03

---
+++ ADO NET 03. Database First Scaffolding/Press.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ADO_NET_03._Database_First_Scaffolding;
+
+public partial class Press
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
---
+++ ADO NET 03. Database First Scaffolding/Program.cs
@@ -0,0 +1,12 @@
+﻿using ADO_NET_03._Database_First_Scaffolding;
+// dotnet tool install --global dotnet-ef - install EF Core .NET CLI
+// dotnet ef dbcontext scaffold "Server=(localdb)\MSSQLLocalDB;Database=Library;Integrated Security=True;Trust Server Certificate=True;" Microsoft.EntityFrameworkCore.SqlServer
+
+
+
+using (var db = new LibraryContext())
+{
+    var author = db.Authors.FirstOrDefault();
+    Console.WriteLine(author.FirstName);
+    Console.WriteLine(author.LastName);
+}
