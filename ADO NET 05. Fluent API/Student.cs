e5c3c4be6d1b3e1a5dc4c80064f8b69c467bf098
Zamanov Nadir Rasim <Zamanov@itstep.edu.az>
2025-08-24 12:08:35 +0400

    ADO 05

--- ADO NET 05. Fluent API/Student.cs
+++ ADO NET 05. Fluent API/Student.cs
@@ -7,6 +7,5 @@ class Student
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public int GroupId { get; set; } // ForeignKey
-
     public virtual Group Group { get; set; } // Navigation property
 }
